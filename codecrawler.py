062223161a06ca15bcc4c99f3a024a333215c76e
vmnguyen <manhnguyen510@gmail.com>
2020-06-15 14:30:48 +0700

    Refactor code

--- codecrawler.py
+++ codecrawler.py
@@ -5,87 +5,82 @@ import re
 import argparse
 from colored import fg, attr
 
-
 def print_banner():
 	print('[!] Manual source code review helper')
 
+def additional_condition(line):
+	#Add more condition here
+	if ("import " in line):
+		return False
+	return True
+
 def grep(filepath, signature):
     regex = ".*" + signature + ".*"
-    regObj = re.compile(regex)
+    reg_obj = re.compile(regex)
     res = []
     count = 0
+
     with open(filepath, encoding="utf8", errors='ignore') as f:
         for line in f:
-            if regObj.match(line):
+            if reg_obj.match(line) and additional_condition(line):
                 res.append("{}" + str(count + 1) +"{}" + ": " + line.replace("{", "").replace("}", "").replace(signature, "{}" + signature + "{}", 1))
             count += 1
     return res
 
 def find_files(path, regex):
-    regObj = re.compile(regex)
+    reg_obj = re.compile(regex)
     res = []
+
     for root, dirs, fnames in os.walk(path):
         for fname in fnames:
             ref_dir = os.path.relpath(root, path)
-            if regObj.match(fname):			
+            if reg_obj.match(fname) and (".svn" not in fname):			
                 res.append(os.path.join(ref_dir, fname))
                 #print(os.path.join(root, fname))
+
     return res
 
-def do_find(signature):
-	global path_to_code
-	global files
+def do_find(signature, path_to_code, files):
 	for file in files:
-		if not (".svn" in file):
-			res = grep(path_to_code + "/" +file, signature)
-			if res != []:
-				tmp = []
-				for i in res:
-					if ("import" not in i):
-						tmp.append(i)
-				if tmp != []:
-					print("Found %s'%s'%s at %s%s%s"  %(fg("yellow"), signature, attr(0), fg("green"), file, attr(0)))
-					for i in tmp:
-						print(i.format(fg("red"), attr(0), fg("yellow"), attr(0)))
+		res = grep(path_to_code + "/" +file, signature)
+		if res != []:
+			print("Found %s'%s'%s at %s%s%s"  %(fg("yellow"), signature, attr(0), fg("green"), file, attr(0)))
+			for match in res:
+				print(match.format(fg("red"), attr(0), fg("yellow"), attr(0)))
+
 def convert_regrex(extension):
 	res = ".*("
 	for i in extension:
 		res += i + "|"
 	res = res[:-1] + ")"
+
 	return res
 
-def load_config():
-	global path_to_config
-	global files
+def find_vuln(path_to_code, path_to_config):
+	print("[!] Finding pattern in your code")
+
 	with open(path_to_config, "r") as config:
 		data = json.load(config)
 		language = "java"
 		vuln = data['language'][language]['vulnerability']
 		extension = data['language'][language]['extension']
 		extension = convert_regrex(extension)
-		#extension = r".*(java|jsp)"
 		files = find_files(path_to_code,extension)
+
 		for i in vuln:
 			patterns = vuln[i]['pattern']
 			for pattern in patterns:
-				file = do_find(pattern)
-				#if (file != []):
-				#	print("[+] Found %s at:" % pattern)
+				do_find(pattern, path_to_code, files)
 
+def main():
+	parser = argparse.ArgumentParser(description="Path to source code folder")
+	parser.add_argument('--path',help="Path to source code folder")
+	parser.add_argument("--config", help="Path to config file")
+	args = parser.parse_args()
+	path_to_code = args.path
+	path_to_config = args.config
+	print_banner()
 
-def find_vuln():
-	print("[!] Finding pattern in your code")
-	config = load_config()
+	find_vuln(path_to_code, path_to_config)
 
-files = []
-parser = argparse.ArgumentParser(description="Path to source code folder")
-parser.add_argument('--path',help="Path to source code folder")
-parser.add_argument("--config", help="Path to config file")
-args = parser.parse_args()
-
-path_to_code = args.path
-print(path_to_code)
-path_to_config = args.config
-print(path_to_config)
-print_banner()
-find_vuln()
+main()
